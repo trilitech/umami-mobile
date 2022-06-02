41cfc5eb916c43d44cfe6ee2c7f546fee0035a2a
Lev Kowalski <levkowalski@gmail.com>
2022-06-02 16:38:02 +0200

    ignore stupid warning

--- src/ignoreWarnings.js
+++ src/ignoreWarnings.js
@@ -3,4 +3,5 @@ import {LogBox} from 'react-native';
 LogBox.ignoreLogs([
   'ViewPropTypes will be removed',
   'ColorPropType will be removed',
+  'Sending `onAnimatedValueUpdate` with no listeners registered',
 ]);
