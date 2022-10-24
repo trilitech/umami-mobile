041d16704932aafaa098835f627af6d08790b003
Lev Kowalski <levkowalski@gmail.com>
2022-10-24 17:56:28 +0200

    remove catalog

--- index.js
+++ index.js
@@ -2,14 +2,8 @@
  * @format
  */
 
-// This is broken
-// import {CATALOG} from '@env';
-
-import Catalog from './Catalog';
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-const CATALOG = false;
-AppRegistry.registerComponent(appName, () => (CATALOG ? Catalog : App));
+AppRegistry.registerComponent(appName, () => App);
