67c011f473f0e0934181ebae70b81ced4dffdfa1
Lev Kowalski <levkowalski@gmail.com>
2022-11-28 22:09:03 +0100

    Jest matchers

--- src/mocks/js/customJestMatchers.js
+++ src/mocks/js/customJestMatchers.js
@@ -2,14 +2,13 @@ import * as Jest from '@glennsl/rescript-jest/src/jest.bs.js';
 
 import {expect} from '@jest/globals';
 
+const dummyAssertion = Jest.Expect.toEqual(Jest.Expect.expect(true), true);
+
 export const toHaveTextContent = (el, str) => {
   expect(el).toHaveTextContent(str);
+  return dummyAssertion;
 };
 export const toHaveProp = (el, prop, value) => {
   expect(el).toHaveProp(prop, value);
+  return dummyAssertion;
 };
-
-export const dummyAssertion = Jest.Expect.toEqual(
-  Jest.Expect.expect(true),
-  true,
-);
